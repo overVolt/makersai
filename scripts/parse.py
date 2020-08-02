2ed3367d16490b3bc723bd0fd52e403f6926ef30
Filippo Pesavento <pesaventofilippo@gmail.com>
2020-08-02 11:52:29 +0200

    Modified parse script

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,20 +1,19 @@
 import json
-import re
-
-def deEmojify(input):
-    regrex_pattern = re.compile(pattern = "["
-        u"\U0001F600-\U0001F64F"
-        u"\U0001F300-\U0001F5FF"
-        u"\U0001F680-\U0001F6FF"
-        u"\U0001F1E0-\U0001F1FF"
-            "]+", flags = re.UNICODE)
-    return regrex_pattern.sub(r'', input)
 
 userBlacklist = [
     365866830,
     235898396,
     208056682,
     417753222
+]
+
+msgBlacklist = [
+    "",
+    "\n",
+    " ",
+    ".",
+    "..",
+    "..."
 ]
 
 print("Loading JSON file...")
@@ -49,6 +48,7 @@ for msg in messages:
 print("Writing to file...")
 with open("output.txt", "w") as out:
     for s in strings:
+        if s in msgBlacklist:
+            continue
         s += "\n"
-        s = deEmojify(s)
         out.write(s)
