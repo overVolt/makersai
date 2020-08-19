c9e05b1b96e260eb895026687d22e32f91a8e920
Filippo Pesavento <pesaventofilippo@gmail.com>
2020-08-19 18:08:51 +0200

    Added admin commands

--- bot/bot.py
+++ bot/bot.py
@@ -10,7 +10,7 @@ with open(join(dirname(abspath(__file__)), "settings.json")) as settings_file:
 
 bot = Bot(settings["token"])
 groupId = int(settings["groupId"])
-generating = False
+generateLock = False
 
 aiConfigPath = join(dirname(abspath(__file__)), f"{settings['aiModelName']}")
 ai = textgenrnn(weights_path=f"{aiConfigPath}_weights.hdf5",
@@ -28,8 +28,14 @@ def generateText():
     )[0]
 
 
+def isAdmin(userId: int, chatId: int):
+    req = bot.getChatAdministrators(chatId)
+    adminList = [int(user["user"]["id"]) for user in req if not user["is_bot"]]
+    return userId in adminList
+
+
 def reply(msg):
-    global generating
+    global generateLock
     chatId = int(msg['chat']['id'])
     fromId = int(msg['from']['id'])
     msgId = int(msg['message_id'])
@@ -48,23 +54,40 @@ def reply(msg):
 
     ## CHAT PRIVATE
     if chatInfo["type"] == "private":
-        bot.sendMessage(chatId, "Sorry, but I only work on the MakersITA group.")
+        bot.sendMessage(chatId, "Sorry, but I only work in the <a href=\"t.me/MakersITA\">MakersITA</a> group.",
+                        parse_mode="HTML", disable_web_page_preview=True)
 
     ## GRUPPI/SUPERGRUPPI
     elif chatInfo["type"] in ["group", "supergroup"]:
         if chatId != groupId: # Only respond to selected group
-            bot.sendMessage(chatId, "Sorry, but I only work on the MakersITA group.")
+            bot.sendMessage(chatId, "Sorry, but I only work in the <a href=\"t.me/MakersITA\">MakersITA</a> group.",
+                            parse_mode="HTML", disable_web_page_preview=True)
+            bot.leaveChat(chatId)
             return
 
         if text == "ping":
             bot.sendMessage(chatId, "pong")
+        elif text == "over":
+            bot.sendMessage(chatId, "Volt!")
 
-        elif text == "/genera":
-            if not generating:
-                generating = True
-                bot.sendChatAction(chatId, "typing")
-                bot.sendMessage(chatId, generateText())
-                generating = False
+        elif text.startswith("/pronuncia ") and isAdmin(fromId, chatId):
+            replyId = msg["reply_to_message"]["message_id"] if "reply_to_message" in msg else None
+            bot.sendMessage(chatId, text.split(" ", 1)[1], parse_mode="HTML",
+                            disable_web_page_preview=True, reply_to_message_id=replyId)
+
+        elif text == "/bloccagen" and isAdmin(fromId, chatId):
+            generateLock = True
+            bot.sendMessage(chatId, "Comando /genera bloccato!")
+
+        elif text == "/sbloccagen" and isAdmin(fromId, chatId):
+            generateLock = False
+            bot.sendMessage(chatId, "Comando /genera sbloccato!")
+
+        elif text == "/genera" and not generateLock:
+            generateLock = True
+            bot.sendChatAction(chatId, "typing")
+            bot.sendMessage(chatId, generateText())
+            generateLock = False
 
 
 def accept_message(msg):
